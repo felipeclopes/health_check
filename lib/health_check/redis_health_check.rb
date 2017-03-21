0e00252106a2215614302990bb165c4e42f2bf75
felipeclopes <felipelopes10@gmail.com>
2017-03-20 17:54:29 -0700

    Added default Rails redis.yml configuration

--- lib/health_check/redis_health_check.rb
+++ lib/health_check/redis_health_check.rb
@@ -1,12 +1,19 @@
 module HealthCheck
   class RedisHealthCheck
     extend BaseHealthCheck
+    
+    REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys[Rails.env.to_sym]
 
     def self.check
       unless defined?(::Redis)
         raise "Wrong configuration. Missing 'redis' gem"
       end
-      res = ::Redis.new.ping
+      res = if REDIS_CONFIG
+        ::Redis.new(REDIS_CONFIG).ping
+      else
+        ::Redis.new.ping
+      end
+      
       res == 'PONG' ? '' : "Redis.ping returned #{res.inspect} instead of PONG"
     rescue Exception => e
       create_error 'redis', e.message
